84f83afd1a633a3c0ce33c40223ba68a53f52114
Daniel Elliott <elliott.daniel.ballantyne@gmail.com>
2019-10-24 13:07:16 -0600

    added ViewPanel tab with basic student table
    Signed-off-by: Daniel Elliott <elliott.daniel.ballantyne@gmail.com>

---
+++ src/Database.java
@@ -0,0 +1,6 @@
+public class Database {
+    public static Object[][] viewTableContents() {
+        Object[][] contents = new Object[2][11];
+        return contents;
+    }
+}
--- src/ViewPanel.java
+++ src/ViewPanel.java
@@ -1,10 +1,24 @@
 import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.Dimension;
 
 public class ViewPanel extends JPanel {
     //variable declaration
-    private JTable studentTable = new JTable(10, 10);
+    private DefaultTableModel studentModel = new DefaultTableModel(Database.viewTableContents(), new String[]{
+            "Student Name", "Education Plan", "College", "Career Path",
+            "Ethnicity", "Regents", "NCAA", "1st Gen", "Gender", "Major", "GPA"}) {
+        @Override
+        public boolean isCellEditable(int row, int column) {
+            return false;
+        }
+    };
+    private JTable studentTable = new JTable(studentModel);
+    private JScrollPane tablePane = new JScrollPane(studentTable);
     //constructor
     public ViewPanel() {
-        add(new JScrollPane(studentTable));
+        studentTable.getColumn("Student Name").setPreferredWidth(120);
+        studentTable.getColumn("Education Plan").setPreferredWidth(130);
+        tablePane.setPreferredSize(new Dimension(1100, 500));
+        add(tablePane);
     }
 }
