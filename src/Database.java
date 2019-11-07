c3d914b181cff123f7026e65d947c1e5603b5aea
Daniel Elliott <elliott.daniel.ballantyne@gmail.com>
2019-11-06 21:27:17 -0700

    working on editwindow
    Signed-off-by: Daniel Elliott <elliott.daniel.ballantyne@gmail.com>

--- src/Database.java
+++ src/Database.java
@@ -25,6 +25,26 @@ public class Database {
         }
         return contents;
     }
+    public static Object[] studentData(int studentNum) {
+        Object[] dbData = new Object[16];
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/dnHbcxGDVx", "dnHbcxGDVx", "QNyBdxwgIA");
+            ResultSet results = con.createStatement().executeQuery("SELECT * FROM students WHERE studentNum = '" + studentNum + "'");
+            while (results.next()) {
+                dbData = new Object[]{results.getString("name"), results.getInt("studentNum"), results.getString("eduPlan"),
+                        results.getString("college"), results.getString("careerPath"), results.getString("ethnicity"),
+                        results.getBoolean("regents"), results.getBoolean("ncaa"), results.getBoolean("firstGen"),
+                        results.getString("gender"), results.getString("major"), results.getDouble("gpa"),
+                        new String[]{results.getString("notes0"), results.getString("notes1"), results.getString("notes2"),
+                            results.getString("notes3")}};
+            }
+        }
+        catch (Exception e) {
+            javax.swing.JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return dbData;
+    }
     public static void executeStatement(String statement) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
--- src/EditWindow.java
+++ src/EditWindow.java
@@ -4,8 +4,40 @@ import java.awt.*;
 public class EditWindow extends JFrame {
     //variable declaration
     private JPanel mainPane = new JPanel();
+    private Object[] studentData;
+    private GroupLayout layout = new GroupLayout(mainPane);
+    private JLabel nameLabel = new JLabel("Name:");
+    private JTextField name = new JTextField();
+    private JLabel studentNumLabel = new JLabel("Student Number:");
+    private JTextField studentNum = new JTextField();
+    private JLabel eduLabel = new JLabel("Education Plan:");
+    private JComboBox eduPlan = new JComboBox(new String[]{
+            "High School Diploma", "Technical Training", "Associate Degree",
+            "Bachelors Degree", "Advanced Degree", "Military", "Other"});
+    private JLabel collegeLabel = new JLabel("College:");
+    private JTextField college = new JTextField();
+    private JLabel careerPathLabel = new JLabel("Career Path:");
+    private JTextField careerPath = new JTextField();
+    private JLabel ethnicityLabel = new JLabel("Ethnicity:");
+    private JComboBox ethnicity = new JComboBox(new String[]{
+            "Hispanic or Latino Origin", "American Indian or Alaska Native", "Asian", "Black or African American",
+            "Native Hawaiian or Other Pacific Islander", "White", "Two or more races"});
+    private JLabel regentsLabel = new JLabel("Regents:");
+    private JCheckBox regents = new JCheckBox();
+    private JLabel ncaaLabel = new JLabel("NCAA:");
+    private JCheckBox ncaa = new JCheckBox();
+    private JLabel firstGenLabel = new JLabel("First Gen:");
+    private JCheckBox firstGen = new JCheckBox();
+    private JLabel genderLabel = new JLabel("Gender:");
+    private JComboBox gender = new JComboBox(new String[]{"Male", "Female", "Other"});
+    private JLabel majorLabel = new JLabel("Major:");
+    private JTextField major = new JTextField();
+    private JLabel gpaLabel = new JLabel("GPA:");
+    private JTextField gpa = new JTextField();
+    private JButton addStudent = new JButton("Add Student");
     //constructor
-    public EditWindow(int studentNum) {
+    public EditWindow(int studentNo) {
+        //set up window
         setPreferredSize(new Dimension(800, 500));
         setMinimumSize(new Dimension(800, 500));
         setResizable(false);
@@ -13,6 +45,82 @@ public class EditWindow extends JFrame {
         setTitle("Edit Student");
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setContentPane(mainPane);
-        System.out.println(studentNum);
+        mainPane.setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+        //get student data from database
+        studentData = Database.studentData(studentNo);
+        //add components to layout and pane
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(nameLabel)
+                        .addComponent(studentNumLabel)
+                        .addComponent(eduLabel)
+                        .addComponent(collegeLabel)
+                        .addComponent(careerPathLabel)
+                        .addComponent(ethnicityLabel)
+                        .addComponent(regentsLabel)
+                        .addComponent(ncaaLabel)
+                        .addComponent(firstGenLabel)
+                        .addComponent(genderLabel)
+                        .addComponent(majorLabel)
+                        .addComponent(gpaLabel))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                        .addComponent(name)
+                        .addComponent(studentNum)
+                        .addComponent(eduPlan)
+                        .addComponent(college)
+                        .addComponent(careerPath)
+                        .addComponent(ethnicity)
+                        .addComponent(regents)
+                        .addComponent(ncaa)
+                        .addComponent(firstGen)
+                        .addComponent(gender)
+                        .addComponent(major)
+                        .addComponent(gpa)
+                        .addComponent(addStudent))
+        );
+        layout.setVerticalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(nameLabel)
+                        .addComponent(name))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(studentNumLabel)
+                        .addComponent(studentNum))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(eduLabel)
+                        .addComponent(eduPlan))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(collegeLabel)
+                        .addComponent(college))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(careerPathLabel)
+                        .addComponent(careerPath))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(ethnicityLabel)
+                        .addComponent(ethnicity))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(regentsLabel)
+                        .addComponent(regents))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(ncaaLabel)
+                        .addComponent(ncaa))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(firstGenLabel)
+                        .addComponent(firstGen))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(genderLabel)
+                        .addComponent(gender))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(majorLabel)
+                        .addComponent(major))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(gpaLabel)
+                        .addComponent(gpa))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER, false)
+                        .addComponent(addStudent))
+        );
+        //transfer student values into form
+
     }
 }
